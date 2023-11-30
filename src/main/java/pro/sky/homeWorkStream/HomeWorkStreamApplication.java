d9c7b803424bde61e7f9c559390133456381ebf8
vadimKapitan <xxxcap13@gmail.com>
2023-11-30 13:16:33 +0300

    Start

---
+++ src/main/java/pro/sky/homeWorkStream/HomeWorkStreamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWorkStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkStreamApplication.class, args);
+	}
+
+}
